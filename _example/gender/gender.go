c30409fe0155eb562e2f9b7b396aa38dde946611
Steve Moyer <smoyer@psu.edu>
2019-03-12 14:06:55 -0400

    Fixes typo

--- _example/gender/gender.go
+++ _example/gender/gender.go
@@ -14,7 +14,7 @@ var genderData = []struct {
 	abbreviation string `enumeration:"lookup"`
 	comment      string
 }{
-	{"NIL", "None", "N", "Nil (uninitialied) gender value"},
+	{"NIL", "None", "N", "Nil (uninitialized) gender value"},
 	{"FEMALE", "Female", "F", "A gender with two X chromosomes"},
 	{"MALE", "Male", "M", "A gender with one X and one Y chromosome"},
 	{"OTHER", "Other", "O", "A gender which chooses not to identify with either of the above"},
